49c5d4b2b1638a0714a88006e882b756b78cea99
safrio <safrio@gmail.com>
2022-03-21 10:23:55 +0500

    load bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '../../assets/stylesheets/application.scss'
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+const $ = require('jquery')
+// @ts-ignore
+window.jQuery = $
+
+require('@rails/ujs').start()
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// Turbolinks.start()
+// ActiveStorage.start()
+require('bootstrap')
+
+require.context('../../assets/images', true);
+
