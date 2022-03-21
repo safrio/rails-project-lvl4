bfb2e7a7fade7ac3b3664f5f6e1cbcc3484b0f86
safrio <safrio@gmail.com>
2022-03-21 10:25:43 +0500

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include AuthConcern
+
+  def nav_menu_item(name, path = '#', options = {})
+    assembled_options = options.merge(class: "nav-link link-dark #{active?(path)}")
+    tag.li class: 'nav-item' do
+      link_to path, assembled_options do
+        tag.div(name)
+      end
+    end
+  end
+
+  def active?(path, options = {})
+    # raise options.inspect
+    if options.key? :active_if
+      'active' if options[:active_if]
+    elsif current_page?(path)
+      'active'
+    end
+  end
 end
