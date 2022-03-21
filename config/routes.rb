ff0d028539e87b874cff2d300ea9e48dfc2104a7
safrio <safrio@gmail.com>
2022-03-21 10:23:20 +0500

    add home route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope module: :web do
+    root 'home#index'
+
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
     delete 'auth', to: 'auth#destroy'
