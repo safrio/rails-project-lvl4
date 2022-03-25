e78f9d738f3b9ba921a8c288d3a861b357930612
safrio <safrio@gmail.com>
2022-03-25 10:07:45 +0500

    add scopes to omni init

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user'
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user,public_repo,admin:repo_hook'
   OmniAuth.config.on_failure = Web::AuthController.action(:failure)
 end
